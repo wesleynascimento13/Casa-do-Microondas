72771c946f2126bd51bfeac913f87fa76b53b807
wesley <wesleyvilgino13@gmail.com>
2024-05-04 23:58:46 -0300

    Atualização das páginas

--- cmo/src/index.js
+++ cmo/src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Saudacao from "./components/saudacao/saudacao.jsx"
+import Header from "./components/header/header.jsx"
+import Footer from "./components/footer/footer.jsx"
 
 // componentização
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <> 
+    <Header marca='Casa do Microondas'/>
+    <Footer marca='Casa do Microondas'/>
     <Saudacao nome='João'/>  
-    <p>Você está na página do projeto Prateleira Digital.</p>
+    <p>Você está na página do projeto Casa do microondas.</p>
+   
+    
   </>
+
 );
 
